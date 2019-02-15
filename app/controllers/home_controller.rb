3cb434d3af9ab121caba409ae5c811b1d2729928
DiegoRenan <renan.ccomp@gmail.com>
2019-02-15 18:58:07 -0300

    Add Home

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  def index
+    @most_downloaded = App.order(downloads: :desc).limit(5)
+    @highest_rated = App.order(rating: :desc).limit(5)
+  end
+end
