8b765dd2166d21023ee779e4b844bdb57f961943
DiegoRenan <renan.ccomp@gmail.com>
2019-02-16 14:49:21 -0300

    add Post

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,6 @@
 class AppsController < ApplicationController
-  
+  before_action :set_app, only: [:show]
+
   # GET /apps
   # GET /apps.json
   def index
@@ -11,8 +12,36 @@ class AppsController < ApplicationController
   def show
   end
 
+  # GET /apps/new
+  def new
+    @app = App.new
+  end
+
+  # POST /apps
+  # POST /apps.json
+  def create
+    @app = App.new(app_params)
+
+    respond_to do |format|
+      if @app.save
+        format.html { redirect_to @app, notice: 'App was successfully created.' }
+        format.json { render :show, status: :created, location: @app }
+      else
+        format.html { render :new }
+        format.json { render json: @app.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def set_app
       @app = App.find(params[:id])
     end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def app_params
+      params.require(:app).permit(:name, :rating, :release_date, :description, 
+            :downloads, :rated_times, :author, :version)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
   root 'home#index'
   
   get 'hello_world', to: 'hello_world#index'
   get '/search', to: 'search#index'
 
-  resources :apps, only: :show
+  resources :apps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
